ee12bb85b5c9a8d2bab7fdf6c4c51834eddce7d2
mdsadikul <mdsadikulislam891@gamil.com>
2022-08-04 17:37:11 +0600

    added comparision js

---
+++ comparition.js
@@ -0,0 +1,28 @@
+// compare 
+var a = 6;
+var b = 10;
+
+// a > b
+console.log( a > b);
+// Output: false
+
+// a < b
+console.log(a < b);
+// Output: true
+
+// a == b
+console.log(a == b);
+// Outpur: false
+
+// a != b
+console.log(a != b);
+// Output: true
+
+// a <= b
+console.log(a <= b);
+// Output: true
+
+// a >= b
+console.log(a >= b);
+// Output: false
+
