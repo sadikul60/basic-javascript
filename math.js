0674bbd8ce56f592b88597e92af41cf86f5e01fb
mdsadikul <mdsadikulislam891@gamil.com>
2022-08-04 17:57:48 +0600

    added math js

---
+++ math.js
@@ -0,0 +1,39 @@
+// Arithmetic operation
+var num1 = 20;
+var num2 = 10;
+
+// addition num1 and num2;
+var sum = num1 + num2;
+console.log(sum);
+// Output: 30
+
+
+// subtraction num1 and num2;
+var sub = num1 - num2;
+console.log(sub);
+// Output: 10
+
+
+// multiplication num1 and num2;
+var mul = num1 * num2;
+console.log(mul);
+// Output: 200
+
+
+// divition num1 and num2;
+var div = num1 / num2;
+console.log(div);
+// Output: 2
+
+
+// modulas or remainder
+var remainder = num1 % num2;
+console.log(remainder);
+// Output: 0
+
+
+var firstName = 'MD.';
+var middleName = 'SADIKUL';
+var lastName = 'ISLAM';
+var add = firstName + ' ' +  middleName + ' ' + lastName;
+console.log(add);
