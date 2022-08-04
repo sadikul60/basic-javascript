0228f1ea28c0bded26da614b63606d186a2ed3c9
mdsadikul <mdsadikulislam891@gamil.com>
2022-08-04 16:54:39 +0600

    added variable

---
+++ variable.js
@@ -0,0 +1,25 @@
+// variable name declaration.
+ // 1. var number = 5;
+ // 2. let number = 5;
+ // 3. const number = 5; 
+
+ // 1. var variable name declaration
+ var number1 = 5;
+ var number2 = 8;
+ var sum = number1 + number2;
+ console.log(sum);
+ // Output: 13
+
+ // 2. let variable name declaration
+ let num1 = 7;
+ let num2 = 8;
+ let total = num1 + num2;
+ console.log(total);
+ // Output: 15
+
+// 3. const variable name declaration
+const a = 12;
+const b = 8;
+const add = a + b;
+console.log(add);
+// Output: 20
