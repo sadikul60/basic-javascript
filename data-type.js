f227af8344e3ba007419b8d361658b9ec67b978c
mdsadikul <mdsadikulislam891@gamil.com>
2022-08-04 17:17:01 +0600

    added data type

---
+++ data-type.js
@@ -0,0 +1,27 @@
+// Type of data tree.
+// 1. Numeric or Number type ;
+// 2. String type;
+// 3. Boolean type;
+
+// 1. Number type data;
+var num = 44;
+console.log(num);
+// Output: 44
+console.log(typeof num);
+// Output: number
+
+
+// 2. String type data;
+var name = 'Asadujjaman';
+console.log(name);
+// Output: Asadujjaman
+console.log(typeof name);
+// Output: string
+
+
+// 3. boolean type data;
+var areYouSure = true;
+console.log(areYouSure);
+// Output: true
+console.log(typeof areYouSure);
+// Output: boolean
